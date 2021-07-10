f7af233aaee5ddd3eb888dde23ce6d9833a08fca
Nia K <nsjkappen@gmail.com>
2021-07-09 19:10:14 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+function add(a, b){
+    return (a + b);
+}
+
+add(1,81);
+
+function subtract(a,b){
+    return (a - b);
+}
+
+subtract(60,40)
+
+function multiply(a,b){
+    return (a*b)
+}
+
+multiply(2,3.4)
+
+
+function divide(a,b){
+    return (a/b)
+}
+
+divide(5.0, 2.5);
+
+
+function increment(n){
+    n++
+    return n;
+}
+
+function decrement(n){
+    n--
+    return n;
+}
+
+function makeInt(n){
+
+    return parseInt(n,10);
+}
+
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
